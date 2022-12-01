30030e6ed8e153caadfc1cd937be3beb5a7f8e21
Farzin Mirzaie <farzin@deriv.com>
2022-12-01 11:34:43 +0800

    refactor: :recycle: update mock data

--- src/index.tsx
+++ src/index.tsx
@@ -16,9 +16,10 @@ export const RemotionRoot: React.FC = () => {
 				height={size}
 				defaultProps={{
 					name: 'Hannibal Hargraves',
-					image: 'https://xsgames.co/randomusers/assets/avatars/male/71.jpg',
+					image:
+						'https://img.freepik.com/premium-photo/cheeky-young-outgoing-handsome-bearded-man-enjoying-awesome-party-inviting-come-checkout-event-pointing-left-recommend-visit-corporate-site-showing-banner-smiling-amused-white-background_176420-55530.jpg?w=2000',
 					achievements:
-						'Achievement A | Achievement B | Achievement C | Achievement D | Achievement E | Achievement F',
+						'Lorem ipsum consectetur adipiscing elit | Sed do eiusmod tempor incididunt | Vulputate dignissim suspendisse | Lobortis scelerisque fermentum duiD | Vitae suscipit tellus mauris a diam maecenas',
 				}}
 			/>
 			<Composition
@@ -33,30 +34,30 @@ export const RemotionRoot: React.FC = () => {
 						{
 							name: 'Addison Alexander',
 							image:
-								'https://xsgames.co/randomusers/assets/avatars/male/37.jpg',
+								'https://thumbs.dreamstime.com/b/keep-our-secret-portait-mysterious-cheeky-bearded-guy-squinting-tricky-smiling-as-showing-shh-gesture-index-finger-133306691.jpg',
 							achievements:
-								'Achievement A | Achievement B | Achievement C | Achievement D | Achievement E | Achievement F',
+								'Lorem ipsum consectetur adipiscing elit | Sed do eiusmod tempor incididunt | Vulputate dignissim suspendisse | Lobortis scelerisque fermentum duiD | Vitae suscipit tellus mauris a diam maecenas',
 						},
 						{
 							name: 'Christian Medina',
 							image:
-								'https://xsgames.co/randomusers/assets/avatars/female/56.jpg',
+								'https://media.istockphoto.com/id/1132932216/photo/close-up-photo-beautiful-amazing-she-her-dark-skin-lady-arm-hand-rock-fan-concert-metal-music.jpg?s=612x612&w=0&k=20&c=ez202gu0hRJi3jJvWFWk6_fT5aYjiQ-43fxaL-vMLfU=',
 							achievements:
-								'Achievement A | Achievement B | Achievement C | Achievement D | Achievement E | Achievement F',
+								'Vulputate dignissim suspendisse | Lobortis scelerisque fermentum duiD | Lorem ipsum consectetur adipiscing elit | Sed do eiusmod tempor incididunt | Vitae suscipit tellus mauris a diam maecenas',
+						},
+						{
+							name: 'Prudence Knowles',
+							image:
+								'https://thumbs.dreamstime.com/b/close-up-photo-amazing-beautiful-her-lady-hands-palms-fingers-point-i-love-myself-i-best-crazy-blinking-wearing-red-knitted-139288025.jpg',
+							achievements:
+								'Sed do eiusmod tempor incididunt | Vulputate dignissim suspendisse | Vitae suscipit tellus mauris a diam maecenas | Lorem ipsum consectetur adipiscing elit | Lobortis scelerisque fermentum duiD',
 						},
 						{
 							name: 'Stephen Maldonado',
 							image:
-								'https://xsgames.co/randomusers/assets/avatars/male/13.jpg',
+								'https://thumbs.dreamstime.com/b/man-shocked-to-see-something-his-side-excited-gesture-copy-space-portrait-young-asian-surprised-smiling-indonesian-158606032.jpg',
 							achievements:
-								'Achievement A | Achievement B | Achievement C | Achievement D | Achievement E | Achievement F',
-						},
-						{
-							name: 'Prudence Knowles							',
-							image:
-								'https://xsgames.co/randomusers/assets/avatars/female/67.jpg',
-							achievements:
-								'Achievement A | Achievement B | Achievement C | Achievement D | Achievement E | Achievement F',
+								'Sed do eiusmod tempor incididunt | Lorem ipsum consectetur adipiscing elit | Vulputate dignissim suspendisse | Vitae suscipit tellus mauris a diam maecenas | Lobortis scelerisque fermentum duiD',
 						},
 					],
 				}}
